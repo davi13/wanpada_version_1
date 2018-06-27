8d0c115c42889207859747faf9ab8a9ed348721c
Alexis Hnr <halexis.92@gmail.com>
2018-06-27 15:41:04 +0200

    navigation

--- frontwanpada/reducer/favoris.reducer.js
+++ frontwanpada/reducer/favoris.reducer.js
@@ -1,14 +1,14 @@
-export default function addFollower(state=[], action) {
-  // L'objet action, contient obligatoirement un type. celui-ci nous permet de définir le type d'action.
-  if (action.type == 'follow') {
-    // Ici, nous récupérons le state.followings précédent pour le sauvegarder dans followings.
-    var followings = [...state];
-    console.log(followings);
-    // Et nous y ajoutons ensuite, le nouveau following sur lequel nous venons de cliquer
-    followings.push({name: action.name, comp: action.comp, id: action.id);
-      return followings
-  } else {
-    // Par défaut, si aucun type d'action n'est défini, followings est un tableau vide.
-    return state
-  }
-}
+// export default function addFollower(state=[], action) {
+//   // L'objet action, contient obligatoirement un type. celui-ci nous permet de définir le type d'action.
+//   if (action.type == 'follow') {
+//     // Ici, nous récupérons le state.followings précédent pour le sauvegarder dans followings.
+//     var followings = [...state];
+//     console.log(followings);
+//     // Et nous y ajoutons ensuite, le nouveau following sur lequel nous venons de cliquer
+//     followings.push({name: action.name, comp: action.comp, id: action.id);
+//       return followings
+//   } else {
+//     // Par défaut, si aucun type d'action n'est défini, followings est un tableau vide.
+//     return state
+//   }
+// }
